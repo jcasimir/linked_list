d251a03fdffe2f19af0dacb34ccfa5e0db57a5d9
Jeff Casimir <jeff@casimircreative.com>
2013-10-23 22:06:08 -0600

    Implemented recursive delete

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -30,6 +30,22 @@ class Node
   def link?
     link
   end
+
+  def delete(match)
+    if link? && link.data == match
+      @link = link.link
+    else
+      link.delete(match)
+    end
+  end
+
+  def to_a
+    if link
+      [data] + link.to_a
+    else
+      [data]
+    end
+  end
 end
 
 class LinkedList < Node
@@ -38,5 +54,13 @@ class LinkedList < Node
 
   def count
     link? ? link.count : 0
+  end
+
+  def to_a
+    if link?
+      link.to_a
+    else
+      []
+    end
   end
 end
--- test/linked_list_test.rb
+++ test/linked_list_test.rb
@@ -4,19 +4,13 @@ require 'minitest/pride'
 require './lib/linked_list'
 
 class LinkedListTest < Minitest::Test
-  def test_it_exists
-    assert LinkedList
-  end
+  attr_reader :list
 
-  def test_it_inserts_an_element
-    list = LinkedList.new
-    assert_equal 0, list.count
-    list.push("hello")
-    assert_equal 1, list.count
+  def setup
+    @list = LinkedList.new
   end
 
   def test_it_inserts_multiple_elements
-    list = LinkedList.new
     assert_equal 0, list.count
     list.push("hello")
     list.push("world")
@@ -24,13 +18,44 @@ class LinkedListTest < Minitest::Test
   end
 
   def test_it_stores_and_retrieves_the_data
-    list = LinkedList.new
     list.push("hello")
     list.push("world")
     assert_equal "world", list.pop
     assert_equal "hello", list.pop
   end
 
+  def test_it_converts_to_an_array_with_multiple_elements
+    list.push("hello")
+    list.push("world")
+    assert_equal ["hello", "world"], list.to_a
+  end
+
+  def test_it_deletes_data
+    list.push("hello")
+    list.push("world")
+    list.push("goes")
+    list.push("boom")
+    assert_equal 4, list.count
+    list.delete("world")
+    assert_equal 3, list.count
+    assert_equal "boom", list.pop
+    assert_equal "goes", list.pop
+    assert_equal "hello", list.pop
+  end
+
+  def test_it_deletes_the_last_element_from_the_list
+    list.push("hello")
+    list.push("world")
+    list.delete("world")
+    assert_equal 1, list.count
+    assert_equal "hello", list.pop
+  end
+
+  def test_it_deletes_the_only_element
+    list.push("hello")
+    list.delete("hello")
+    assert_equal 0, list.count
+  end
 end
 
 class NodeTest < Minitest::Test
