80cde287822201d3aac1186f1a5c33cd178a53ba
Jeff Casimir <jeff@casimircreative.com>
2013-10-23 21:50:28 -0600

    initial implementation of push

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,3 +1,60 @@
 class LinkedList
-  
+  attr_reader :head
+
+  def push(data)
+    if head
+      head.push(data)
+    else
+      @head = Node.new(data)
+    end
+  end
+
+  def pop
+    if head.link?
+      head.pop
+    else
+      result = head.data
+      @head = nil
+      return result
+    end
+  end
+
+  def count
+    return 0 unless head
+    head.count
+  end
+end
+
+class Node
+  attr_reader :link, :data
+
+  def initialize(data)
+    @data = data
+  end
+
+  def count
+    link ? 1 + link.count : 1
+  end
+
+  def push(data)
+    if link
+      link.push(data)
+    else
+      @link = Node.new(data)
+    end
+  end
+
+  def pop
+    if link && link.link?
+      link.pop
+    else
+      output = link.data
+      @link = nil
+      return output
+    end
+  end
+
+  def link?
+    link
+  end
 end
--- test/linked_list_test.rb
+++ test/linked_list_test.rb
@@ -8,4 +8,53 @@ class LinkedListTest < Minitest::Test
     assert LinkedList
   end
 
+  def test_it_inserts_an_element
+    list = LinkedList.new
+    assert_equal 0, list.count
+    list.push("hello")
+    assert_equal 1, list.count
+  end
+
+  def test_it_inserts_multiple_elements
+    list = LinkedList.new
+    assert_equal 0, list.count
+    list.push("hello")
+    list.push("world")
+    assert_equal 2, list.count
+  end
+
+  def test_it_stores_and_retrieves_the_data
+    list = LinkedList.new
+    list.push("hello")
+    list.push("world")
+    assert_equal "world", list.pop
+    assert_equal "hello", list.pop
+  end
+end
+
+class NodeTest < Minitest::Test
+  def test_it_counts_one_when_there_is_no_link
+    node = Node.new("hi")
+    assert_equal 1, node.count
+  end
+
+  def test_it_counts_one_more_than_links_count
+    node = Node.new("hi")
+    node.push("world")
+    assert_equal 2, node.count
+  end
+
+  def test_it_pops_the_link_when_there_are_no_other_children
+    node = Node.new("hi")
+    node.push("world")
+    assert_equal "world", node.pop
+  end
+
+  def test_pop_removes_the_last_element
+    node = Node.new("hi")
+    node.push("world")
+    assert_equal 2, node.count
+    node.pop
+    assert_equal 1, node.count
+  end
 end
